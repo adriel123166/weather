0840480af5c2c66853ddd7eadf9595ddc94410c2
felix <felixadriel123@gmail.com>
2025-11-20 19:26:01 +0800

    Initial commit: Weather API with 13 endpoints

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Weather API',
+      version: '1.0.0',
+      description: 'API for weather data management'
+    },
+    servers: [{ url: `http://localhost:${PORT}` }]
+  },
+  apis: ['./server.js']
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 
 // Weather schema
